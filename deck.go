52ca178913e6bc793e44b1b68289babc8a76f64f
David Lam <DavidLam@DecisionResearch.com>
2018-04-02 21:57:33 -1000

    Completed newDeck, deal functio

--- deck.go
+++ deck.go
@@ -6,8 +6,25 @@ import "fmt"
 //which is a slice of strings
 type deck []string
 
-func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "2"} // , "3", "4"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
 	}
+	//fmt.Println(cards)
+	return cards
+}
+
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spacdes")
+import "fmt"
 
-	cards.print()
+func main() {
+	//cards := deck{"Ace of Diamonds", newCard()}
+	//cards = append(cards, "Six of Spacdes")
+
+	cards := newDeck()
+
+	//cards.print()
+	hand, remaining := deal(cards, 2)
+	hand.print()
+	fmt.Println("=========")
+	remaining.print()
+
 }
 
 func newCard() string {
